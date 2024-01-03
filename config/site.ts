bed5ebc8a846b34dee37df8a58ba342e977497fd
chrishanthacosta <64259523+chrishanthacosta@users.noreply.github.com>
2024-01-03 08:47:48 +0530

    title changed

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "Aga wasi",
+	description: "Invest in a bright future",
 	navItems: [
 		{
 			label: "Home",
