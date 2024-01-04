11291741d684d0a08c0593b261fc2a6610444439
chrishanthacosta <64259523+chrishanthacosta@users.noreply.github.com>
2024-01-04 09:09:27 +0530

    logo updated

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,9 @@ export default function Home() {
 			</section>
 
 			<section className="flex  items-center  justify-around  flex-wrap gap-2 sm:gap-0 lg:flex-nowrap mt-8   rounded-md"> 
-				<Card1 header="විශ්වාසනීය සේවය" iconpath="hshake.svg" body="දිගු කාලීන ව්‍යාපාරික අත්දැකීම් මත ගොඩනැගුනු විස්වාසය අපගේ ප්‍රධානම වත්කමයි"></Card1>
-				<Card1 header="ස්ථීර ආදායමක්" iconpath="income.svg" body="නිති පතා ඔබ වෙත ලැබෙන ආදායම නිසා අවදානම අවම වීම"></Card1>
-				<Card1 header="නිරවුල් කර්මාන්තයක්" iconpath="clear.svg" body="පරිසර හිතකාමී කර්මාන්තයක මුදල් ආයෝජනය මගින් ඔබ ලබන තෘප්තිය "></Card1>
+				<Card1 header="විශ්වාසනීය සේවය" iconpath="hshake.svg" body="දිගු කාලීන ව්‍යාපාරික අත්දැකීම් මත ගොඩනැගුනු විශ්වාසය අපගේ ප්‍රධානම වත්කමයි"></Card1>
+				<Card1 header="ස්ථීර ආදායමක්" iconpath="income.svg" body="නිති පතා ඔබ වෙත ලැබෙන ආදායම නිසා අවදානම අවම‍යි"></Card1>
+				<Card1 header="නිරවුල් කර්මාන්තයක්" iconpath="clear.svg" body="පරිසර හිතකාමී නිශ්පාදන කර්මාන්තයක මුදල් ආයෝජනය මගින් ඔබ ලබන තෘප්තිය. "></Card1>
 
 			</section>
 		</>
--- components/landing-page-compos/hero.tsx
+++ components/landing-page-compos/hero.tsx
@@ -10,7 +10,7 @@ const noto = Noto_Sans_Sinhala({
 const Hero = () => {
   return (
     <div className={`${noto.className}  text-center text-xl sm:text-2xl text-purple-800 p-6  m-12   bg-slate-200 rounded-md  sm:w-[40%] h-full  w-screen   sm:h-[360px]`}>
-      <span className="sm:text-4xl block font-bold">හරිත අයෝජනයකට අප හා එක්වන්න!</span>
+      <span className="sm:text-4xl block font-bold">හරිත ආයෝජනයකට අප හා එක්වන්න!</span>
       <span>විදේශීය වෙළදපොල සදහා නිපදවන අති දුර්ලභ</span>
       <span> මිල අධික සුවද විලවුන් සදහා භාවිතා වන අමුද්‍රව්‍ය නිපදවන ව්‍යාපෘ</span>
       <span>තියක ආයෝජනය කර අනාගතය සාර්ථක කර ගන්න.</span>
